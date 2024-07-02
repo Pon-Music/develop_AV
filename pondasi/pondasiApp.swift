91bc768bdbd6eafcd9f036bf41af629f5123b403
kazukihashimotokazuki <abkeybgeks@gmail.com>
2024-07-02 16:39:01 +0900

    Initial Commit

---
+++ pondasi/pondasiApp.swift
@@ -0,0 +1,17 @@
+//
+//  pondasiApp.swift
+//  pondasi
+//
+//  Created by 和希 on 2024/07/02.
+//
+
+import SwiftUI
+
+@main
+struct pondasiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
